76584f1b0441d0b6333083f52a9cd6641d940649
Afnan Enayet <afnan.enayet@gmail.com>
2017-08-02 12:32:06 -0400

    Created data models for surveys

--- piqd_site/surveys/models.py
+++ piqd_site/surveys/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Data models for the suveys app
+
+class Survey(models.Model):
+    """The overarching model for a survey, which includes a set of questions 
+    and some corresponding answers"""
+    pub_date = models.DateTimeField("date published")
+
+class Question(models.Model):
+    """This is the data model for a question, multiple Questions are associated
+    with a Survey in a many-to-one relationship"""
+    survey = models.ForeignKey(Survey, on_delete = models.Cascade)
+    question_text = models.CharField(max_length = 2000)
+
+class Choice(models.Model):
+    """An answer to a question (a multiple choice answer), multiple choices 
+    are associated with a Question in a many-to-one relationship"""
+    question = models.ForeignKey(Question, on_delete = models.Cascade)
+    choice_text = models.CharField(max_length = 2000)
+    votes = models.IntegerField(default = 0)
+
